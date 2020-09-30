4d317a84773c47f1fb47f5ff660502abb1714b48
Your Name <you@example.com>
2020-09-30 19:32:37 +0300

    server lisatty

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Moikka moi!")
+console.log("tämä koodi on 100% egologinen")
+
+const express = require('express')
+const app = express()
+const port = 3000
+app.listen(3000, () => console.log("kuuntelen"));
+app.use(express.static("public"));
+
+console.log("serveri_kaynnissa")
